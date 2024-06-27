032d9c34701237ebd50d4ac009b211cccb421400
ehotigas <90466869+ehotigas@users.noreply.github.com>
2024-06-27 07:10:54 -0300

    update: API_URL

--- src/app/types/GlobalVars.ts
+++ src/app/types/GlobalVars.ts
@@ -1,2 +1,2 @@
-// export const API_URL = "http://172.20.74.21:5001";
-export const API_URL = "http://localhost:5001";
+export const API_URL = "http://172.20.74.21:5001";
+// export const API_URL = "http://localhost:5001";
